f2932793078a52e11413111bfd8e99b949957df1
chaeyoung103 <thdcodud103@naver.com>
2025-08-05 11:18:02 +0900

    design: global css default setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ko'>
+    <html lang='ko' data-foxified=''>
       <body className={` antialiased`}>{children}</body>
     </html>
   );
