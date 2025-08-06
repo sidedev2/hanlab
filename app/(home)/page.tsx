8b7d6a1792adc242a0ba1fba26210ca0b8914f02
chaeyoung103 <thdcodud103@naver.com>
2025-08-06 10:57:50 +0900

    feat: 메인 페이지들 추가

---
+++ app/(home)/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className='font-extrabold h-dvh flex justify-center items-center'>
+      HOME
+    </div>
+  );
+};
+
+export default Home;
---
+++ app/members/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div className='font-extrabold h-dvh flex justify-center items-center'>
+      MEMBERS
+    </div>
+  );
+};
+
+export default Page;
