af2ac4c01d657a1d55c8d60cce3ffcea0418aa39
chaeyoung103 <thdcodud103@naver.com>
2025-08-06 13:32:29 +0900

    refactor: nav 바 배열 이용한 map 방식으로 변경

--- components/common/SidebarWithNav.tsx
+++ components/common/SidebarWithNav.tsx
@@ -6,51 +6,27 @@ import Link from 'next/link';
 
 const SidebarWithNav = () => {
   const { closeSidebar } = useSidebar();
+  const navs = [
+    'home',
+    'research',
+    'publications',
+    'members',
+    'gallery',
+    'contact',
+  ];
   return (
     <Sidebar>
       <nav className='flex flex-col gap-4 p-4'>
-        <Link
-          href='/'
-          className='text-black text-[1.6rem] hover:underline'
-          onClick={closeSidebar}
-        >
-          home
-        </Link>
-        <Link
-          href='/research'
-          className='text-black text-[1.6rem] hover:underline'
-          onClick={closeSidebar}
-        >
-          research
-        </Link>
-        <Link
-          href='/publications'
-          className='text-black text-[1.6rem] hover:underline'
-          onClick={closeSidebar}
-        >
-          publications
-        </Link>
-        <Link
-          href='/members'
-          className='text-black text-[1.6rem] hover:underline'
-          onClick={closeSidebar}
-        >
-          members
-        </Link>
-        <Link
-          href='/gallery'
-          className='text-black text-[1.6rem] hover:underline'
-          onClick={closeSidebar}
-        >
-          gallery
-        </Link>
-        <Link
-          href='/contact'
-          className='text-black text-[1.6rem] hover:underline'
-          onClick={closeSidebar}
-        >
-          contact
-        </Link>
+        {navs.map((nav) => (
+          <Link
+            key={nav}
+            href={nav === 'home' ? '/' : `/${nav}`}
+            className='text-black text-[1.6rem] hover:underline'
+            onClick={closeSidebar}
+          >
+            {nav}
+          </Link>
+        ))}
       </nav>
     </Sidebar>
   );
