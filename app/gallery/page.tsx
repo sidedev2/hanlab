f84c678fb177bd6da2a4324af2349fd22bc20fdb
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-09-29 14:15:10 +0900

    feat: gallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,9 +1,25 @@
 import { SubHeader } from '@/components';
+import GalleryCard from '@/components/gallery/GalleryCard';
+import { galleryData } from '@/constants/galleryData';
 
 const Page = () => {
   return (
-    <div className='flex flex-col items-center justify-start'>
-      <SubHeader title='Gallery' />
+    <div>
+      <div className='flex flex-col items-center justify-start'>
+        <SubHeader title='Gallery' />
+      </div>
+      <div className='mx-8 my-8 lg:mx-30 lg:my-16'>
+        <div className='grid grid-cols-1 gap-7 lg:grid-cols-3 lg:gap-15'>
+          {galleryData.map((item) => (
+            <GalleryCard
+              key={item.id}
+              src={item.src}
+              title={item.title}
+              date={item.date}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ constants/galleryData.ts
@@ -0,0 +1,38 @@
+export const galleryData = [
+  {
+    id: 1,
+    src: '/assets/hanlab_main.png',
+    title: 'Gallery Image 1',
+    date: '2025.09.29',
+  },
+  {
+    id: 2,
+    src: '/assets/hanlab_main.png',
+    title: 'Gallery Image 2',
+    date: '2025.09.28',
+  },
+  {
+    id: 3,
+    src: '/assets/hanlab_main.png',
+    title: 'Gallery Image 3',
+    date: '2025.09.27',
+  },
+  {
+    id: 4,
+    src: '/assets/hanlab_main.png',
+    title: 'Gallery Image 4',
+    date: '2025.09.26',
+  },
+  {
+    id: 5,
+    src: '/assets/hanlab_main.png',
+    title: 'Gallery Image 5',
+    date: '2025.09.25',
+  },
+  {
+    id: 6,
+    src: '/assets/hanlab_main.png',
+    title: 'Gallery Image 6',
+    date: '2025.09.24',
+  },
+];
