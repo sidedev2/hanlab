b6df42172d5fbdb4285351a00bad6112bb065657
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-09-30 20:41:38 +0900

    feat: supabase 설정

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,7 +1,9 @@
+import { getGalleryItems } from '@/api/gallary';
 import { SubHeader, GalleryCard } from '@/components';
-import { galleryData } from '@/constants/galleryData';
 
-const Page = () => {
+const Page = async () => {
+  const items = await getGalleryItems();
+
   return (
     <div>
       <div className='flex flex-col items-center justify-start'>
@@ -9,12 +11,12 @@ const Page = () => {
       </div>
       <div className='mx-8 my-8 lg:mx-30 lg:my-16'>
         <div className='grid grid-cols-1 gap-7 lg:grid-cols-3 lg:gap-15'>
-          {galleryData.map((item) => (
+          {items.map((item) => (
             <GalleryCard
               key={item.id}
-              src={item.src}
-              title={item.title}
-              date={item.date}
+              src={item.src ?? ''}
+              title={item.title ?? ''}
+              date={item.date ?? ''}
             />
           ))}
         </div>
